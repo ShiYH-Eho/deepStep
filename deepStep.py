f9d47c364be8986aac98060b577580eb1566ca73
石英昊 <shiyh15@mails.tsinghua.edu.cn>
2019-04-09 00:16:36 +0800

    trace2speed

--- deepStep.py
+++ deepStep.py
@@ -81,12 +81,8 @@ b_fc2 = bias_variable([H*W])
 #y = tf.nn.softmax(tf.matmul(h_drop, W_fc2) + b_fc2)
 
 
-y = tf.nn.relu(tf.matmul(h_drop, W_fc2) + b_fc2)
+y = tf.matmul(h_drop, W_fc2) + b_fc2 + 0.5
 
-#loss = tf.sqrt(tf.divide(tf.reduce_sum(tf.square(tf.subtract(y,y_))),tf.reduce_sum(y_)))
-#loss = tf.divide(tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(y,y_)))),tf.reduce_sum(y_))
-#loss = tf.divide(tf.reduce_sum(tf.divide(tf.abs(tf.subtract(y,y_)),y_)),batchSize)
-#loss = tf.divide(tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(y,y_)))),tf.reduce_sum(y_))
 loss = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y,y_))))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 accuracy = 1 - tf.divide(tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(y,y_)))),tf.reduce_sum(y_))
@@ -103,18 +99,3 @@ for i in range(5000):
 		print "step %d, training loss %g"%(i, loss_accuracy / batchSize)
 	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})	
 
-#print "test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
-
-'''
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-sess.run(tf.initialize_all_variables())
-for i in range(1000):
-	batch = mnist.train.next_batch(50)
-	if i%100 == 0:
-		train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-		print "step %d, training accuracy %g"%(i, train_accuracy)
-	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})	
-
-print "test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
-'''
